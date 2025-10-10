75b2c6f96d3b37ce6811516b8ba2a1596529f648
singhvigyat <code.vigyat@gmail.com>
2025-10-11 01:31:51 +0530

    feat(frontend): add glassmorphic effect to navbar

--- frontend/src/components/dashboard.tsx
+++ frontend/src/components/dashboard.tsx
@@ -13,7 +13,7 @@ export const Dashboard = ({ orgs }: any) => {
     return (
         <div>
             {/* navbar */}
-            <nav className="  justify-between  w-screen fixed top-0 h-16 z-10 border-b-1 flex items-center px-16 tracking-tighter">
+            <nav className=" backdrop-blur-md justify-between  w-screen fixed top-0 h-16 z-10 border-b-1 flex items-center px-16 tracking-tighter">
                 <div className="font-satoshi-black">
                     nameoftheproject
                 </div>
@@ -43,51 +43,46 @@ export const Dashboard = ({ orgs }: any) => {
             </nav>
 
             {/* orgs */}
-            <div className="pt-16  w-screen h-screen border-r-1  ">
-                <div className="flex flex-wrap content-start bg-white w-[calc(100%-20%)] p-4 overflow-y-auto h-full 
+            <div className="w-fit h-screen overflow-y-auto
   [&::-webkit-scrollbar]:w-2
   [&::-webkit-scrollbar-track]:rounded-full
   [&::-webkit-scrollbar-track]:bg-gray-100
   [&::-webkit-scrollbar-thumb]:rounded-full
   [&::-webkit-scrollbar-thumb]:bg-gray-300
   dark:[&::-webkit-scrollbar-track]:bg-neutral-700 
-  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 ">
-                    {/* <Card orgName='Second ORg of the year' tagline='this is the new stuff enjoynenssdfk' tags={["advanced data structures", "first", "no", "c++", "his is hti new first", "thle"]} />
-                    <Card orgName='Second ORg of the year' tagline='this is the new stuff enjoynenssdfk' tags={["advanced data structures", "first", "no", "c++", "his is hti new first", "thle"]} /> */}
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+                <div className="flex flex-wrap content-start w-[calc(100%-20%)] pt-20 p-4">
 
                     {
-                        // Object.entries(orgs).map(([_, value]:any)=>{
-                        //     return Object.entries(value).map(([k ,v]:any)=>{
-                        //         return <div key={k}>{JSON.stringify(v.orgName)}</div>
-                        //     })
-                        // })
-
                         Object.entries(orgs).map(([_, e]: any) => {
-                            return Object.entries(e).map(([keys, value]: any) => {
-                                return <Card logoUrl={value.logoUrl} orgName={value.orgName} tagline={value.tagLine} tags={value.techContent} />
-                                return <div key={keys}>{JSON.stringify(value.orgName)}</div>
-                            })
+                            // return Object.entries(e).map(([keys, value]: any) => {
+                            return <Card key={_} logoUrl={e.logoUrl} orgName={e.orgName} tagline={e.tagLine} tags={e.techContent || []} />
+                            // return <div key={keys}>{JSON.stringify(value)}</div>
+                            // })
                         })
-
                     }
 
 
                 </div>
-                <div className=" w-1/5 h-full fixed right-0 top-16 z-20 border-l-1 ">
-                    {/* filters */}
-                    <div className="h-  20">
 
-                    </div>
-                    <div className="mt-0 h-full flex items-start p-3 font-satoshi-regular">
-                        <AccordionSideBar />
-                    </div>
+            </div>
+            {/* Sidebar */}
+            <div className=" w-1/5 h-full fixed right-0 top-16 z-20  ">
+                {/* filters */}
+                <div className="h-  20">
 
-                    {/* links */}
-                    <div className="bg-red-200 justify-center flex items-center absolute bottom-0 h-[8%] mb-16 w-full border-t-2">
-                        links
-                    </div>
+                </div>
+                <div className="mt-0 h-full flex items-start p-3 font-satoshi-regular">
+                    <AccordionSideBar />
+                </div>
+
+                {/* links */}
+                <div className="bg-red-200 justify-center flex items-center absolute bottom-0 h-[8%] mb-16 w-full border-t-2">
+                    links
                 </div>
             </div>
+
+
         </div>
     )
 }
